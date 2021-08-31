3429970a96093306136a88ff2657fda51fe52bb8
Rodgers31 <otienor986@gmail.com>
2021-08-31 17:11:51 -0400

    finished first 2 get statements

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,47 @@
 // implement your posts router here
+
+const express = require('express');
+
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+// | 1 | GET    | /api/posts              | Returns **an array of all the post objects** contained in the database
+router.get('/', (req, res) => {
+	Post.find(req.body)
+		.then((posts) => {
+			res.status(200).json(posts);
+		})
+		.catch((error) => {
+			res.status(500).json({
+				message: 'The posts information could not be retrieved',
+				error: error.message,
+			});
+		});
+});
+// | 2 | GET    | /api/posts/:id          | Returns **the post object with the specified id**
+
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.then((post) => {
+			if (!post) {
+				res.status(404).json({
+					message: 'The post with the specified ID does not exist',
+				});
+			} else {
+				res.status(200).json(post);
+			}
+		})
+		.catch((error) => {
+			res.status(500).json({
+				message: 'The post information could not be retrieved',
+				error: error.message,
+			});
+		});
+});
+// | 3 | POST   | /api/posts              | Creates a post using the information sent inside the request body and returns **the newly created post object**
+// | 4 | PUT    | /api/posts/:id          | Updates the post with the specified id using data from the request body and **returns the modified document**, not the original |
+// | 5 | DELETE | /api/posts/:id          | Removes the post with the specified id and returns the **deleted post object**
+// | 6 | GET    | /api/posts/:id/comments | Returns an **array of all the comment objects** associated with the post with the specified id
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+module.exports = server;
